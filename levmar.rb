345ecb4d5b3a321ea01eb63b61998b2d8b69d615
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2014-09-11 13:55:07 +0900

    Added levmar formula
    Added formula for the levmar library which implements Levenberg–Marquardt fitting (http://users.ics.forth.gr/~lourakis/levmar/)
    
    Signed-off-by: Joseph Barker <joseph.barker@imr.tohoku.ac.jp>

---
+++ levmar.rb
@@ -0,0 +1,15 @@
+require "formula"
+
+class Levmar < Formula
+  homepage "http://users.ics.forth.gr/~lourakis/levmar/"
+  url "http://users.ics.forth.gr/~lourakis/levmar/levmar-2.6.tgz"
+  sha1 "118bd20b55ab828d875f1b752cb5e1238258950b"
+
+  # depends_on "cmake" => :build
+  depends_on "homebrew/headonly/f2c" # if your formula requires any X11/XQuartz components
+
+  def install
+    system "make"
+    lib.install "liblevmar.a"
+  end
+end
