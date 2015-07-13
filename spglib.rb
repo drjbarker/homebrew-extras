734d07e9a8e6405786e6a297773e6646800d51d8
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2015-07-14 08:29:18 +0900

    Update spglib to 1.8.1
    Signed-off-by: Joseph Barker <joseph.barker@imr.tohoku.ac.jp>

--- spglib.rb
+++ spglib.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Spglib < Formula
   homepage "http://spglib.sourceforge.net/"
-  url "http://sourceforge.net/projects/spglib/files/spglib/spglib-1.6/spglib-1.6.1.tar.gz"
-  sha1 "b32b512257e1a2129dd02903545223aea58c2d42"
+  url "http://sourceforge.net/projects/spglib/files/spglib/spglib-1.8/spglib-1.8.1.tar.gz"
+  sha1 "fd82be485ca2a77f9a9c3ff4b75e3ae4547270c4"
 
   def install
 
