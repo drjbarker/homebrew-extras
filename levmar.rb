57e35011f8bf09d8fcffcbb5b5cd04370158194f
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2017-01-05 14:21:08 +0900

    Update levmar hash

--- levmar.rb
+++ levmar.rb
@@ -3,7 +3,7 @@ require "formula"
 class Levmar < Formula
   homepage "http://users.ics.forth.gr/~lourakis/levmar/"
   url "http://users.ics.forth.gr/~lourakis/levmar/levmar-2.6.tgz"
-  sha1 "118bd20b55ab828d875f1b752cb5e1238258950b"
+  sha256 "8ccd493c93a5b032189175b174c2ad23a1636e64a839899ba2956cb6cd751683"
 
   # depends_on "cmake" => :build
   depends_on "homebrew/headonly/f2c" # if your formula requires any X11/XQuartz components
