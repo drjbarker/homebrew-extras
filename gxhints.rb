da9c253ad1931a0fad51b2976daae07d77db15a2
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2017-01-05 15:43:12 +0900

    Correct gxhints hash

--- gxhints.rb
+++ gxhints.rb
@@ -3,7 +3,7 @@ require "formula"
 class Gxhints < Formula
   homepage "http://sourceforge.net/projects/gxhints/"
   url "http://downloads.sourceforge.net/project/gxhints/gxhints/gxhints-1.1.5/gxhints-1.1.5.tar.gz"
-  sha256 "380eac9c19e6be29da75339c8d892d17712f41e4ac611b4de1f498c52bb27942"
+  sha256 "b436fbcda86e6a8466a67296f3ab14dfb79f4a78e24db3107f16f74915fbad30"
 
   depends_on "batik"
   depends_on "ghostscript"
