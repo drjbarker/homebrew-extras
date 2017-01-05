c3e48469b92a05214424b9490477b8edbb53d45f
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2017-01-05 14:30:49 +0900

    Use non deprecated f2c

--- levmar.rb
+++ levmar.rb
@@ -3,10 +3,10 @@ require "formula"
 class Levmar < Formula
   homepage "http://users.ics.forth.gr/~lourakis/levmar/"
   url "http://users.ics.forth.gr/~lourakis/levmar/levmar-2.6.tgz"
-  sha256 "8ccd493c93a5b032189175b174c2ad23a1636e64a839899ba2956cb6cd751683"
+  sha256 "3bf4ef1ea4475ded5315e8d8fc992a725f2e7940a74ca3b0f9029d9e6e94bad7"
 
   # depends_on "cmake" => :build
-  depends_on "homebrew/headonly/f2c" # if your formula requires any X11/XQuartz components
+  depends_on "homebrew/boneyard/f2c" # if your formula requires any X11/XQuartz components
 
   def install
     system "make"
