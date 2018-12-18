e320d513ed58cb6cb12a28a7dec68e613ab1ffff
Joseph Barker <j.barker@leeds.ac.uk>
2018-12-18 11:25:59 +0000

    Updates pyvtk formula syntax for python

--- pyvtk.rb
+++ pyvtk.rb
@@ -1,11 +1,9 @@
-require "formula"
-
 class Pyvtk < Formula
   homepage "https://pypi.python.org/pypi/PyVTK"
   url "https://pypi.python.org/packages/source/P/PyVTK/PyVTK-0.4.85.tar.gz"
   sha256 "4f971e21eb73b44fd4dd52bed1aeea7879e0d9e8debfded651bb8fc9c80033e2"
 
-  depends_on :python
+  depends_on "python@2" if MacOS.version <= :snow_leopard
 
   def install
     system "python", "setup.py",
